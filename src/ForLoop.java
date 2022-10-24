789f852b00440e171413175b4bba7e22a005b0f6
abdulhamidll <abdulhamid1998uzmobile@gamil.com>
2022-10-24 11:41:01 +0500

    First commit

---
+++ src/ForLoop.java
@@ -0,0 +1,7 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int count = 0; count < 9; count ++){
+            System.out.println("salom");
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String data = same(5);
+        System.out.println(data);
+    }
+    public static String same(int n){
+        if (n > 0){
+            return "musbat son";
+        } else {
+                return "manfiy son";
+        }
+    }
+}
